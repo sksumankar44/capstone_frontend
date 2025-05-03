a60b6190a289321b8d233dfecbe33ffa9b25ab29
suman.kar <suman.kar@comviva.com>
2025-05-03 16:47:34 +0530

    Modified:Changes in supabase url and token

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://loozfdikordwzcdgcugu.supabase.co";
+const supabaseUrl = "https://tcekkinkvrffxvguugiq.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxvb3pmZGlrb3Jkd3pjZGdjdWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1MDI2NTYsImV4cCI6MjAzNjA3ODY1Nn0.94JkQe_zJK6kttQqR4E1aHj87f13VFEoUKfRgnHEvC8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWtraW5rdnJmZnh2Z3V1Z2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4ODcyMDksImV4cCI6MjA1ODQ2MzIwOX0.fpzZeCL2CDkBmkOcoB7wnuQjD2M5kZcgHyBkB1PL4qk";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
